c3427d7cbcaffa395fb1569be0884d5f4ea6dd5b
ferdiu <ferdiu.manzella@gmail.com>
2026-02-11 11:01:25 +0100

    version bump
    Signed-off-by: ferdiu <ferdiu.manzella@gmail.com>

--- src/similarity_matrix/main.py
+++ src/similarity_matrix/main.py
@@ -43,7 +43,7 @@ def main():
     parser.add_argument(
         '--version',
         action='version',
-        version='0.1.20')
+        version='0.2.0')
     parser.add_argument(
         '--debug',
         action='store_true',
